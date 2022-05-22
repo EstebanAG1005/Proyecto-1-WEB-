b67485688d59e4e935346024ea816567a86e98f8
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-05-21 22:11:33 -0600

    cambios

--- src/components/Pagina.jsx
+++ src/components/Pagina.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Video from '../../public/Videos/Leon.mp4'
+import Oveja from '../../public/Img/Oveja.jpg'
 
 const Pagina = () => {
 
     return(
         <div>
-            <h1>Prueba con React</h1>
             <video autoPlay loop muted className='VideoFondo'>
-                <source src={Video} type="video/mp4"/>
-            </video>
+                    <source src={Video} type="video/mp4"/>
+                </video>
+            <div>
+                <img src={Oveja} alt="Oveja" />;
+            </div>
+            <div>
+                
+            </div>
         </div>
+        
     )
 }
 
