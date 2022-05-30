adb1433a3e3a54639e4187f6074318e2dda96ac1
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-05-30 10:55:26 -0600

    Se agrega News

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/navbar.js';
 import Pagina from './components/Pagina.js';
 import Musica from './components/Musica.js'
+import News from './components/News.js';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Navbar />
       <Pagina/>
       <Musica/>
+      <News/>
     </>
   );
 }
